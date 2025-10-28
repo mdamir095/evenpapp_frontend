ae80b135599de28c15b6ea07befbeeae43597404
akkaliyar <ajay@whiz-solutions.com>
2025-10-28 11:40:02 +0530

    update file

--- src/components/common/width.tsx
+++ src/components/common/width.tsx
@@ -179,61 +179,6 @@ module.exports = {
   //       // ... and so on up to '50': '50%'
   //     },
   // }
-  extend: {
-      colors: {
-        background: "hsl(var(--background))",
-        foreground: "hsl(var(--foreground))",
-        border: "hsl(var(--border))",
-        primary: {
-          DEFAULT: "hsl(var(--primary))",
-          accent: "hsl(var(--primary-accent))",
-          foreground: "hsl(var(--primary-foreground))",
-        },
-        secondary: {
-          DEFAULT: "hsl(var(--secondary))",
-          accent: "hsl(var(--secondary-accent))",
-          foreground: "hsl(var(--secondary-foreground))",
-        },
-        error: {
-          DEFAULT: "hsl(var(--error))",
-          accent: "hsl(var(--error-accent))",
-          foreground: "hsl(var(--error-foreground))",
-        },
-        info: {
-          DEFAULT: "hsl(var(--info))",
-          accent: "hsl(var(--info-accent))",
-          foreground: "hsl(var(--info-foreground))",
-        },
-        success: {
-          DEFAULT: "hsl(var(--success))",
-          accent: "hsl(var(--success-accent))",
-          foreground: "hsl(var(--success-foreground))",
-        },
-        warning: {
-          DEFAULT: "hsl(var(--warning))",
-          accent: "hsl(var(--warning-accent))",
-          foreground: "hsl(var(--warning-foreground))",
-        },
-      },
-      fontFamily: {
-        sans: ["IBM Plex Sans", "sans-serif"],
-      },
-      keyframes: {
-        "accordion-down": {
-          from: { height: "0" },
-          to: { height: "var(--radix-accordion-content-height)" },
-        },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: "0" },
-        },
-      },
-      animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
-      },
-    },
-  },
   plugins: [
     require("@tailwindcss/forms"),
     require("tailwindcss-animate"),
